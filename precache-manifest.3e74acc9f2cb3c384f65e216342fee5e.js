3d50e914ba18b95b0d89ead98006b1f20700c1c2
Ross Hutchens <rlhcxm@gmail.com>
2020-08-24 04:32:19 -0500

    Updates

---
+++ precache-manifest.3e74acc9f2cb3c384f65e216342fee5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf1043351eb92810c313e1440c945f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89d202b5a45cba0727c6",
+    "url": "./static/css/main.9bd14713.chunk.css"
+  },
+  {
+    "revision": "4c9e6e8637f5566efba8",
+    "url": "./static/js/2.fae4c882.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fae4c882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d202b5a45cba0727c6",
+    "url": "./static/js/main.282cc8f6.chunk.js"
+  },
+  {
+    "revision": "4d6d7d8a922ed1f5e305",
+    "url": "./static/js/runtime-main.ec0258f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rlh74.github.io.git/precache-manifest.a4b456b537c016980150dba7fc411511.js"
+  "./precache-manifest.3e74acc9f2cb3c384f65e216342fee5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rlh74.github.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
