dcc7e4ac7b1d22196fbac7c4b8d087a6f8ac346f
Ross Hutchens <rlhcxm@gmail.com>
2020-08-24 03:45:10 -0500

    initial commit, copied from project folder from another working directory

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
-/** ---------- EXPRESS ROUTES ---------- **/
-// app.use('/');
-
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
     console.log('Listening on port: ', PORT);
