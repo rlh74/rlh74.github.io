14dec44b6a12eeacafec26304b1ec0f91ebe18e3
Ross Hutchens <rlhcxm@gmail.com>
2020-08-24 05:00:07 -0500

    Changed text on about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ class About extends Component {
                 Here, I am using P5.js which is a client-side JavaScript library of Processing, a coding tool developed for visual artists built with Java.
                 The figure is composed by the modulating the amplitude of a signal (x) and a carrier (y), of which the respective variables are then modulated by a pair of frequencies -- in this example I am swapping the frequencies used for the signal and carrier input.<br/><br/>
                 I chose two frequencies with an intervalic relationship that I frequent with audio synthesis -- it is interesting to witness the visual representation of that relationship!
-                To create the animation effect, I am incrementally increasing the phase of the signal frequency and resetting it to 0 as it approaches 360.
+                To create the illusion of the figure rotating, I am incrementally increasing the phase of the signal frequency and resetting it to 0 as it approaches 360.
 
             </div>  
             </div>
